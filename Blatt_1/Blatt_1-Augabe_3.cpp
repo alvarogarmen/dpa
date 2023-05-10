6c937bb15ac078d0e26eabb3934ec90a42620176
AlvaroUbuntu <alvar@AlvaroPC.localdomain>
2023-05-10 15:48:45 +0200

    new config

--- Blatt_1/Blatt_1-Augabe_3.cpp
+++ Blatt_1/Blatt_1-Augabe_3.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
 #include <random>
+#include <fstream>
+#include <chrono>
 #include <mpi.h>
 
-int main(int argc, char** argv) {
-    int numPoints = 1000000; // Number of points to generate
+double estimatePi(int numPoints, int rank, int numProcesses) {
+    // Counter for the number of points in the unit circle
     int numPointsInsideCircle = 0;
-    int rank, numProcesses;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+    // Random number generator setup. Each process gets one generator
+    std::random_device rd;
+    std::mt19937 gen(rd() + rank);
+    std::uniform_real_distribution<double> dis(-1.0, 1.0);
 
     int pointsPerProcess = numPoints / numProcesses;
     int localCount = 0;
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dis(-1.0, 1.0);
 
     for (int i = 0; i < pointsPerProcess; ++i) {
         double x = dis(gen);
@@ -27,11 +24,49 @@ int main(int argc, char** argv) {
         }
     }
 
-    MPI_Reduce(&localCount, &numPointsInsideCircle, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Sum the local counts from all processes
+    int totalPointsInsideCircle;
+    MPI_Reduce(&localCount, &totalPointsInsideCircle, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        double piApprox = 4.0 * numPointsInsideCircle / numPoints;
-        std::cout << "Estimated value of pi: " << piApprox << std::endl;
+        return 4.0 * totalPointsInsideCircle / numPoints;
+    }
+
+    return 0.0;
+}
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, numProcesses;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+
+    if (rank == 0) {
+        std::ofstream outputFile;
+        outputFile.open("running_time3.csv");  // Open file to write running time data
+        outputFile << "numPoints,runningTime\n";  // Write header for the CSV file
+
+        std::cout << "Running time data will be written to running_time3.csv" << std::endl;
+
+        for (int numPoints = 10000; numPoints <= 1000000; numPoints += 10000) {
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+            double estimatedPi = estimatePi(numPoints, rank, numProcesses);
+
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            std::chrono::duration<double> elapsedSeconds = end - begin;
+
+            outputFile << numPoints << "," << elapsedSeconds.count() << "\n";
+        }
+
+        outputFile.close();  // Close the file
+
+        std::cout << "Running time data has been written to running_time2.csv" << std::endl;
+    } else {
+        for (int numPoints = 10000; numPoints <= 1000000; numPoints += 10000) {
+            estimatePi(numPoints, rank, numProcesses);
+        }
     }
 
     MPI_Finalize();
